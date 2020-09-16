5419a6f11870ab4cd8009395ebfa9a0d235b008a
arqcarloseduardo <carloseduardo.arq@gmail.com>
2020-09-16 08:45:29 -0300

    Erros tooltip finalizados

--- goBarber/gobarber-web-app/src/components/Tooltip/index.tsx
+++ goBarber/gobarber-web-app/src/components/Tooltip/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom'
+import React from 'react'
 
 import { Container } from './styles'
 
@@ -7,7 +7,7 @@ interface TooltipProps {
   className?: string
 }
 
-const Tooltip: React.FC<TooltipProps> = ({title, className = '', children}) => {
+const Tooltip: React.FC<TooltipProps> = ({title, className="", children}) => {
   return (
     <Container className={className}>
       {children}
